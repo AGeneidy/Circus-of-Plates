8924c5f3ffdd4d7a889f1c6452ebe5eed41f32a4
Ayman Geneidy <Ayman Geneidy@AymanGeneidy>
2013-12-09 06:31:21 +0200

    Player Class is adjusted

--- Circus Of Plates/src/Player.java
+++ Circus Of Plates/src/Player.java
@@ -1,4 +1,20 @@
+import java.util.ArrayList;
 
 public class Player {
+	ArrayList<Shape> rightHand;
+	ArrayList<Shape> leftHand;
+
+	Player() {
+		rightHand = new ArrayList<Shape>();
+		leftHand = new ArrayList<Shape>();
+	}
+
+	void appendRight(Shape p) {
+		rightHand.add(p);
+	}
+
+	void appendLeft(Shape p) {
+		leftHand.add(p);
+	}
 
 }
