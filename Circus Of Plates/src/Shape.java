bcf53a739e754296f944e08f6c40725e3750a606
Ayman Geneidy <Ayman Geneidy@AymanGeneidy>
2013-12-09 07:06:17 +0200

    State Pattern

--- Circus Of Plates/src/Shape.java
+++ Circus Of Plates/src/Shape.java
@@ -1,4 +1,14 @@
-
 public class Shape {
+
+	private State state;
+
+	public void setState(State startState) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public State getState() {
+		return state;
+	}
 
 }
