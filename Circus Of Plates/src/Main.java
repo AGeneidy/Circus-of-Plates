e8b751479c38ad82c0a5bc9ae9907ecaa0a90ac9
Ayman Geneidy <Ayman Geneidy@AymanGeneidy>
2013-12-09 05:02:09 +0200

    Main Classes

--- Circus Of Plates/src/Main.java
+++ Circus Of Plates/src/Main.java
@@ -1,8 +1,5 @@
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("test");
-		System.out.println("ana 7oda");
-		System.out.println("test");
 		System.out.println("saba7");
 	}
 }
