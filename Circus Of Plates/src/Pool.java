dd60db77d614a995eca6b1ab8f9c222fd8129284
Ayman Geneidy <Ayman Geneidy@AymanGeneidy>
2013-12-09 06:51:29 +0200

    dsf

--- Circus Of Plates/src/Pool.java
+++ Circus Of Plates/src/Pool.java
@@ -1,8 +1,8 @@
 import java.util.ArrayList;
 
 public class Pool {
-	ArrayList<Shape> used;
-	ArrayList<Shape> available;
+	static ArrayList<Shape> used;
+	static ArrayList<Shape> available;
 
 	public Pool() {
 		used = new ArrayList<Shape>();
@@ -12,13 +12,17 @@ public class Pool {
 			available.add(x.getRandomShape());
 	}
 
-
 	public Shape getShape() {
-		if(available.isEmpty())return null;
-		Shape a = available.get(available.size()-1);
-		available.remove(available.size()-1);
+		if (available.isEmpty())
+			return null;
+		Shape a = available.get(available.size() - 1);
+		available.remove(available.size() - 1);
 		used.add(a);
 		return a;
+	}
+
+	public static ArrayList<Shape> getUsed() {
+		return used;
 	}
 
 }
