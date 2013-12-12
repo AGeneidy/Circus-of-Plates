d3d47a2dbb2e30cd027030ae4ee9568d1ac41d7e
eless_000 <eless_000@BODA>
2013-12-12 15:44:38 +0200

    a

--- Circus Of Plates/src/objectGenerator/PlateFactory.java
+++ Circus Of Plates/src/objectGenerator/PlateFactory.java
@@ -4,12 +4,19 @@ public class PlateFactory extends AbstractFactory{
 	private static PlateFactory plateFactory ;
 	private PlateFactory(){}
 	
+	protected static PlateFactory getPlateFactory() {
+		// TODO Auto-generated method stub
+		if(plateFactory==null)
+			return plateFactory = new PlateFactory();
+		else return plateFactory;
+	}
+
 	@Override
 	Plate getPlate(String shapeType) {
 		if (shapeType == null){
 			return null;
-		}else if(shapeType.equalsIgnoreCase("PLATE")){
-			return new Plate();
+		}else if(shapeType.equalsIgnoreCase("OVAL")){
+			return new OvalPlate();
 		}
 		
 		return null;
@@ -18,11 +25,5 @@ public class PlateFactory extends AbstractFactory{
 	@Override
 	Player getPlayer() {
 		return null;
-	}
-	protected static PlateFactory getPlateFactory() {
-		// TODO Auto-generated method stub
-		if(plateFactory==null)return plateFactory=new PlateFactory();
-		else return plateFactory;
-
 	}
 }
--- Circus Of Plates/src/objectGenerator/PlayerFactory.java
+++ Circus Of Plates/src/objectGenerator/PlayerFactory.java
@@ -2,7 +2,14 @@ package objectGenerator;
 
 public class PlayerFactory extends AbstractFactory {
 	private static PlayerFactory playerFactory;
-
+	
+	protected static PlayerFactory getPlayerFactory() {
+		if (playerFactory == null)
+			return playerFactory = new PlayerFactory();
+		else
+			return playerFactory;
+	}
+	
 	private PlayerFactory() {
 	}
 
@@ -14,13 +21,6 @@ public class PlayerFactory extends AbstractFactory {
 	@Override
 	Player getPlayer() {
 		return new Player();
-	}
-
-	protected static PlayerFactory getPlayerFactory() {
-		if (playerFactory == null)
-			return playerFactory = new PlayerFactory();
-		else
-			return playerFactory;
 	}
 
 }
