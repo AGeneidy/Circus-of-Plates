c61cb992c76818bf612b4e88dfabba52c8fcbbe5
Ayman Geneidy <Ayman Geneidy@AymanGeneidy>
2013-12-13 16:43:08 +0200

    player

--- Circus Of Plates/src/objectGenerator/Plate.java
+++ Circus Of Plates/src/objectGenerator/Plate.java
@@ -1,11 +1,40 @@
 package objectGenerator;
 
+import java.awt.Color;
+import java.awt.Point;
+
 public class Plate {
 	private int id;
+	private Color plateColor;
+	private Point position;
+	private int height, width;
+
+	public int getHeight() {
+		return height;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public Point getPosition() {
+		return position;
+	}
+
+	public void setPosition(Point position) {
+		this.position = position;
+	}
+
+	public Color getPlateColor() {
+		return plateColor;
+	}
+
+	public void setPlateColor(Color plateColor) {
+		this.plateColor = plateColor;
+	}
 
 	public Plate() {
 		// TODO Auto-generated constructor stub
-		id = 0;
 	}
 
 	public void setID(int i) {
